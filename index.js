2635a5504df9b87ff629f49d666691ca02646776
patsicko <patsicko@g>
2022-08-04 06:31:29 +0200

    Feat:setTimout

---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+
+  
+    var backgroundImg=["./bag.jpeg","./cart.png","./hat.jpeg","./laptop.jpg","./phone.jpeg","./shoes.jpg"
+]
+
+
+let i=0;
+
+function changeImage() {   
+
+document.body.style.backgroundImage = `url(${backgroundImg[i]})`;
+
+setTimeout(()=>{
+i++;
+changeImage();
+},2000)
+
+
+
+document.body.style.backgroundRepeat="no-repeat";
+document.body.style.height="100%";
+
+
+document.body.style.backgroundSize="500px";
+}
+
+changeImage();
+
+
+
+
+
+const list=document.querySelectorAll(".list");
+console.log(list);
+
+let active=0;
+
+const change=()=>{
+
+list[active].style.backgroundColor="red";
+
+setTimeout(()=>{
+if(active<5){
+active++;
+}
+else{
+active=0;
+active++;
+}
+
+change();
+
+},2000)
+}
+
+change();
+
+
+
+
+
